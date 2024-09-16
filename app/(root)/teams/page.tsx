85b4d3022d9f5ef0ac18a223e43d059ed572d8be
Aliyan Sajid <aliyansajid127@gmail.com>
2024-09-16 17:23:58 +0500

    Fix TypeScript module error in app/(root)/notes/page.tsx

---
+++ app/(root)/teams/page.tsx
@@ -0,0 +1,13 @@
+import SectionHeader from "@/components/SectionHeader";
+import TopHeader from "@/components/TopHeader";
+
+const Teams = () => {
+  return (
+    <section>
+      <TopHeader />
+      <SectionHeader title="Messages"></SectionHeader>
+    </section>
+  );
+};
+
+export default Teams;
---
+++ app/(root)/users/page.tsx
@@ -0,0 +1,13 @@
+import SectionHeader from "@/components/SectionHeader";
+import TopHeader from "@/components/TopHeader";
+
+const Users = () => {
+  return (
+    <section>
+      <TopHeader />
+      <SectionHeader title="Messages"></SectionHeader>
+    </section>
+  );
+};
+
+export default Users;
